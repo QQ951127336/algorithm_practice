d9ce6cc98445793fe41297cebf792e0482175043
max <951127336@qq.com>
2019-03-28 23:24:56 +0800

    leetcode922.按奇偶排序数组 II

---
+++ CppPractice/CppPractice/sort_array_by_party_ii.h
@@ -0,0 +1,29 @@
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> sortArrayByParityII(vector<int>& A) {
+		vector<int> _double;
+		vector<int> _single;
+		for (int i = 0; i < A.size(); i++) {
+			if (A[i] % 2 == 0)
+				_double.push_back(A[i]);
+			else {
+				_single.push_back(A[i]);
+			}
+		}
+		vector<int> result;
+		int doubleIndex = 0, singleIndex = 0;
+		for (int i = 0; i < A.size(); i++) {
+			if (i % 2 == 0) {
+				result.push_back(_double[doubleIndex++]);
+			}
+			else {
+				result.push_back(_single[singleIndex++]);
+			}
+		}
+		return result;
+	}
+};
