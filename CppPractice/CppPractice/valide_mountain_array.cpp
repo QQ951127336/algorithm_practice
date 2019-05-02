60fa5baceb327e45e9d436a4546148dd96cd0f81
max <951127336@qq.com>
2019-05-02 16:18:00 +0800

    leetcode 941

---
+++ CppPractice/CppPractice/valide_mountain_array.cpp
@@ -0,0 +1,38 @@
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+	bool validMountainArray(vector<int>& A) {
+		if (A.size() < 3)
+			return false;
+		bool right = false;
+		bool left = false;
+
+		for (int i = 0; i < A.size() - 1; i++) {
+			if (A[i] > A[i + 1]) {
+				if (!left) {
+					return false;
+				}
+				else {
+					right = true;
+				}
+			}
+			else if (A[i] < A[i + 1]) {
+				if (right)
+					return false;
+				else {
+					left = true;
+				}
+			}
+			else {
+				return false;
+			}
+		}
+		if (left && right)
+			return true;
+		else
+			return false;
+	}
+};
